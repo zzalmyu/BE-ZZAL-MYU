1f6545b73f74a8fe1f7042bc6bd4fb8a1642c937
suzzingv <sjlim1999@naver.com>
2024-01-29 22:36:53 +0900

    [FEAT] user repository

---
+++ src/main/java/com/prgrms/be/domain/user/infrastructure/UserJPARepository.java
@@ -0,0 +1,16 @@
+package com.prgrms.be.domain.user.infrastructure;
+
+import com.prgrms.be.domain.user.domain.entity.User;
+import com.prgrms.be.domain.user.domain.enums.SocialType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserJPARepository extends JpaRepository<User, Long> {
+
+    Optional<User> findBySocialTypeAndSocialId(SocialType socialType, String socialId);
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByNickname(String nickname);
+}
