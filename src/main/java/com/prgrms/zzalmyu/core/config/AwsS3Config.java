48de363fcdf84c8f1f3714ec383895ed82ffd074
voidmelody <sss9073@naver.com>
2024-02-08 15:57:28 +0900

    [FEAT] 생성자 주입으로 변경

--- src/main/java/com/prgrms/zzalmyu/core/config/AwsS3Config.java
+++ src/main/java/com/prgrms/zzalmyu/core/config/AwsS3Config.java
@@ -7,20 +7,21 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+
 
 @Configuration
-@PropertySource("classpath:application.yml")
 public class AwsS3Config {
+    private final String accessKey;
+    private final String secretKey;
+    private final String region;
 
-    @Value("${aws.credentials.access}")
-    private String accessKey;
-
-    @Value("${aws.credentials.secret}")
-    private String secretKey;
-
-    @Value("${aws.region.static}")
-    private String region;
+    public AwsS3Config(@Value("${aws.credentials.access}") String accessKey,
+                       @Value("${aws.credentials.secret}") String secretKey,
+                       @Value("${aws.region.static}") String region) {
+        this.accessKey = accessKey;
+        this.secretKey = secretKey;
+        this.region = region;
+    }
 
     @Bean
     public BasicAWSCredentials basicAWSCredentials() {
