c34a14da6b7b28f4facc9010c1abb407f3674dca
voidmelody <sss9073@naver.com>
2024-02-08 13:23:41 +0900

    [REFACT] S3 삭제 컨트롤러 반환 타입 변경

--- src/main/java/com/prgrms/zzalmyu/domain/image/presentation/controller/AwsS3Controller.java
+++ src/main/java/com/prgrms/zzalmyu/domain/image/presentation/controller/AwsS3Controller.java
@@ -4,6 +4,8 @@ import com.prgrms.zzalmyu.domain.image.application.AwsS3Service;
 import com.prgrms.zzalmyu.domain.image.presentation.dto.res.AwsS3RequestDto;
 import com.prgrms.zzalmyu.domain.image.presentation.dto.res.AwsS3ResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -22,7 +24,8 @@ public class AwsS3Controller {
     }
 
     @DeleteMapping("/resource")
-    public void remove(@RequestBody AwsS3RequestDto awsS3RequestDto) {
+    public ResponseEntity remove(@RequestBody AwsS3RequestDto awsS3RequestDto) {
         awsS3Service.remove(awsS3RequestDto);
+        return ResponseEntity.ok(HttpStatus.OK);
     }
 }
