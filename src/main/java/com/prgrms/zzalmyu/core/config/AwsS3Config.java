0a75bdf9a369169b32a98220b3a2bbace843e42b
voidmelody <sss9073@naver.com>
2024-02-08 15:53:54 +0900

    [FEAT] @propertysource 추가

--- src/main/java/com/prgrms/zzalmyu/core/config/AwsS3Config.java
+++ src/main/java/com/prgrms/zzalmyu/core/config/AwsS3Config.java
@@ -7,8 +7,10 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:application.yml")
 public class AwsS3Config {
 
     @Value("${aws.credentials.access}")
