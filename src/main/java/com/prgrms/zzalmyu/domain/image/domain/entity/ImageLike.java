73f1cd5f1c17c0d67f848df8ac7aad1e7e890b81
YongSang Kim <109174778+YongNyeo@users.noreply.github.com>
2024-02-11 16:54:04 +0900

    [REFACT] 매핑테이블에 @ManyToOne 추가 (#14)
    * [REFACT] 매핑테이블에 @ManyToOne 추가

--- src/main/java/com/prgrms/zzalmyu/domain/image/domain/entity/ImageLike.java
+++ src/main/java/com/prgrms/zzalmyu/domain/image/domain/entity/ImageLike.java
@@ -1,11 +1,7 @@
 package com.prgrms.zzalmyu.domain.image.domain.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.prgrms.zzalmyu.domain.user.domain.entity.User;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,15 +17,17 @@ public class ImageLike {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "image_id", nullable = false)
-    private Long imageId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "image_id", nullable = false)
+    private Image image;
 
-    @Column(name = "user_id", nullable = false)
-    private Long userId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
     @Builder
-    private ImageLike(Long imageId, Long userId) {
-        this.imageId = imageId;
-        this.userId = userId;
+    private ImageLike(Image image, User user) {
+        this.image = image;
+        this.user = user;
     }
 }
--- src/main/java/com/prgrms/zzalmyu/domain/image/domain/entity/ImageTag.java
+++ src/main/java/com/prgrms/zzalmyu/domain/image/domain/entity/ImageTag.java
@@ -1,11 +1,7 @@
 package com.prgrms.zzalmyu.domain.image.domain.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.prgrms.zzalmyu.domain.tag.domain.entity.Tag;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,15 +17,17 @@ public class ImageTag {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "image_id", nullable = false)
-    private Long imageId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "image_id", nullable = false)
+    private Image image;
 
-    @Column(name = "tag_id", nullable = false)
-    private Long tagId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tag_id", nullable = false)
+    private Tag tag;
 
     @Builder
-    private ImageTag(Long imageId, Long tagId) {
-        this.imageId = imageId;
-        this.tagId = tagId;
+    private ImageTag(Image image, Tag tag) {
+        this.image = image;
+        this.tag = tag;
     }
 }
