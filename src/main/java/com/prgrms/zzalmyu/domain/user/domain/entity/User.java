cfa1a72361176db2a7118f9e0a8b73a1fcf4c2e7
suzzingv <84590065+suzzingv@users.noreply.github.com>
2024-02-13 23:40:17 +0900

    [REFACT] user 권한에 한정되지 않은 권한 관리

--- src/main/java/com/prgrms/zzalmyu/domain/user/domain/entity/User.java
+++ src/main/java/com/prgrms/zzalmyu/domain/user/domain/entity/User.java
@@ -69,7 +69,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.toString()));
+        return List.of(new SimpleGrantedAuthority(role.getKey()));
     }
 
     @Override
