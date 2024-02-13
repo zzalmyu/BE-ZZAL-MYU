ca043b2b94b6704fa246b677f3b5daa10297f89f
suzzingv <84590065+suzzingv@users.noreply.github.com>
2024-02-13 09:07:22 +0900

    [REFACT] h2 설정

--- src/main/java/com/prgrms/zzalmyu/core/config/SecurityConfig.java
+++ src/main/java/com/prgrms/zzalmyu/core/config/SecurityConfig.java
@@ -1,7 +1,7 @@
 package com.prgrms.zzalmyu.core.config;
 
 import com.prgrms.zzalmyu.domain.user.application.RedisService;
-import com.prgrms.zzalmyu.domain.user.infrastructure.UserJPARepository;
+import com.prgrms.zzalmyu.domain.user.infrastructure.UserRepository;
 import com.prgrms.zzalmyu.domain.user.jwt.filter.JwtAuthenticationProcessingFilter;
 import com.prgrms.zzalmyu.domain.user.jwt.service.JwtService;
 import com.prgrms.zzalmyu.domain.user.oauth2.handler.OAuth2LoginFailureHandler;
@@ -25,7 +25,7 @@ public class SecurityConfig {
 
     private final JwtService jwtService;
     private final RedisService redisService;
-    private final UserJPARepository userJPARepository;
+    private final UserRepository userRepository;
     private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
     private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
     private final CustomOAuth2UserService customOAuth2UserService;
@@ -42,6 +42,7 @@ public class SecurityConfig {
                 httpSecuritySessionManagementConfigurer.sessionCreationPolicy(
                     SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/h2-console/*").permitAll()
                 .requestMatchers("/api/v1/user/jwt-test").authenticated()
                 .requestMatchers("/**").permitAll())
             .oauth2Login(oauth2 -> oauth2
@@ -57,6 +58,6 @@ public class SecurityConfig {
 
     @Bean
     public JwtAuthenticationProcessingFilter jwtAuthenticationProcessFilter() {
-        return new JwtAuthenticationProcessingFilter(jwtService, redisService, userJPARepository);
+        return new JwtAuthenticationProcessingFilter(jwtService, redisService, userRepository);
     }
 }
