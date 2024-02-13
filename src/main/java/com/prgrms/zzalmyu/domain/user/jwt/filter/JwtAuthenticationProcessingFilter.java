7e06e9a43f8814ffaad221dd363b85a395419401
suzzing_V <84590065+suzzingv@users.noreply.github.com>
2024-02-13 09:20:08 +0900

    Feat/#9 user logout (#10)
    * [FEAT] logout 기능 구현
    
    * [FEAT] 필터 예외처리
    
    * [FIX] 충돌 수정

--- src/main/java/com/prgrms/zzalmyu/domain/user/jwt/filter/JwtAuthenticationProcessingFilter.java
+++ src/main/java/com/prgrms/zzalmyu/domain/user/jwt/filter/JwtAuthenticationProcessingFilter.java
@@ -27,16 +27,23 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
     private final RedisService redisService;
     private final UserJPARepository userJPARepository;
 
+    private static String NO_CHECK_URL = "/api/v1/user/logout";
+
     private GrantedAuthoritiesMapper authoritiesMapper = new NullAuthoritiesMapper();
 
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
         FilterChain filterChain) throws ServletException, IOException {
+        if (request.getRequestURI().equals(NO_CHECK_URL)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        checkLogout(request); //로그아웃한 사용자면 인증 처리 안함
 
         String refreshToken = jwtService.extractRefreshToken(request)
             .filter(jwtService::isTokenValid)
-            .orElse(null); // 리프레시 토큰 만료 기간이 끝나 유효성 검사에서 걸려서 null이 되었을 경우
+            .orElse(null); // TODO: 리프레시 토큰 만료 기간이 끝나 유효성 검사에서 걸려서 null이 되었을 경우
 
         if (refreshToken != null) {
             checkRefreshTokenAndReissueAccessToken(response, refreshToken);
@@ -44,6 +51,16 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
         }
 
         checkAccessTokenAndAuthentication(request, response, filterChain);
+    }
+
+    private void checkLogout(HttpServletRequest request) {
+        jwtService.extractAccessToken(request).ifPresent(accessToken -> {
+            String value = redisService.getValues(accessToken);
+
+            if (value.equals("logout")) {
+                throw new UserException(ErrorCode.SECURITY_UNAUTHORIZED);
+            }
+        });
     }
 
     private void checkRefreshTokenAndReissueAccessToken(HttpServletResponse response,
@@ -65,8 +82,7 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
 
     private String reissueRefreshToken(String email) {
         String reissuedRefreshToken = jwtService.createRefreshToken();
-        redisService.setValues(reissuedRefreshToken, email,
-            Duration.ofMillis(jwtService.getRefreshTokenExpirationPeriod()));
+        jwtService.updateRefreshToken(reissuedRefreshToken, email);
         return reissuedRefreshToken;
     }
 
