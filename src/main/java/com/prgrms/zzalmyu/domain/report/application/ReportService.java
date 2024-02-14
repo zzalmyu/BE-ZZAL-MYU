cc7cee4bc6078dba20772c2a2d6f946351ef84ba
suzzingv <sjlim1999@naver.com>
2024-02-14 16:54:33 +0900

    [REFACT] 신고 상세 정보 응답에 태그 정보 추가

--- src/main/java/com/prgrms/zzalmyu/domain/report/application/ReportService.java
+++ src/main/java/com/prgrms/zzalmyu/domain/report/application/ReportService.java
@@ -2,10 +2,13 @@ package com.prgrms.zzalmyu.domain.report.application;
 
 import com.prgrms.zzalmyu.core.properties.ErrorCode;
 import com.prgrms.zzalmyu.domain.image.application.ImageRemoveService;
+import com.prgrms.zzalmyu.domain.image.infrastructure.ImageRepository;
 import com.prgrms.zzalmyu.domain.report.domain.entity.Report;
 import com.prgrms.zzalmyu.domain.report.exception.ReportException;
 import com.prgrms.zzalmyu.domain.report.infrastructure.ReportRepository;
 import com.prgrms.zzalmyu.domain.report.presentation.dto.response.ReportDetailResponse;
+import com.prgrms.zzalmyu.domain.tag.domain.entity.Tag;
+import com.prgrms.zzalmyu.domain.tag.presentation.dto.res.TagResponseDto;
 import com.prgrms.zzalmyu.domain.user.application.UserService;
 import com.prgrms.zzalmyu.domain.user.domain.entity.User;
 
@@ -24,6 +27,7 @@ public class ReportService {
     private final ReportRepository reportRepository;
     private final UserService userService;
     private final ImageRemoveService imageRemoveService;
+    private final ImageRepository imageRepository;
 
     public void reportImage(Long userId, Long imageId) {
         Report report = Report.builder()
@@ -43,7 +47,8 @@ public class ReportService {
         List<ReportDetailResponse> responses = reports.stream()
             .map(report -> {
                 User reportUser = userService.findUserById(report.getId());
-                return ReportDetailResponse.of(report, reportUser);
+                List<TagResponseDto> tags = getTags(report.getImageId());
+                return ReportDetailResponse.of(report, reportUser, tags);
             })
             .collect(Collectors.toList());
 
@@ -57,5 +62,12 @@ public class ReportService {
         } else {
             throw new ReportException(ErrorCode. IMAGE_DELETION_NOT_ALLOWED);
         }
+    }
+
+    private List<TagResponseDto> getTags(Long imageId) {
+        List<Tag> tags = imageRepository.findTagsByImageId(imageId);
+        return tags.stream()
+                .map(tag -> new TagResponseDto(tag.getId(), tag.getName()))
+                .collect(Collectors.toList());
     }
 }
