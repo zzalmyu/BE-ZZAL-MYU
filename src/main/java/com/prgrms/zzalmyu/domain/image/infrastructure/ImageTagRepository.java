fd340f75211943fe6d52607be832621fc4f6928e
YongNyeo <won05121@gmail.com>
2024-02-14 11:12:57 +0900

    [FEAT] ImageLikeRepository , ImageTagRepository 메서드 추가

--- src/main/java/com/prgrms/zzalmyu/domain/image/infrastructure/ImageTagRepository.java
+++ src/main/java/com/prgrms/zzalmyu/domain/image/infrastructure/ImageTagRepository.java
@@ -3,5 +3,8 @@ package com.prgrms.zzalmyu.domain.image.infrastructure;
 import com.prgrms.zzalmyu.domain.image.domain.entity.ImageTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ImageTagRepository extends JpaRepository<ImageTag, Long>, ImageTagRepositoryCustom {
+    List<Long> findImageTagIdsByImageId(Long imageId);
 }
