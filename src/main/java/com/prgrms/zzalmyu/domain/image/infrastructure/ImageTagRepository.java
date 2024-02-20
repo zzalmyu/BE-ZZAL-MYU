fb2d703cdf945527362fc7cb99d72933f77f87d8
YongNyeo <won05121@gmail.com>
2024-02-20 22:58:21 +0900

    [REFACT] ImageTagRepository 수정

--- src/main/java/com/prgrms/zzalmyu/domain/image/infrastructure/ImageTagRepository.java
+++ src/main/java/com/prgrms/zzalmyu/domain/image/infrastructure/ImageTagRepository.java
@@ -2,9 +2,12 @@ package com.prgrms.zzalmyu.domain.image.infrastructure;
 
 import com.prgrms.zzalmyu.domain.image.domain.entity.ImageTag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ImageTagRepository extends JpaRepository<ImageTag, Long>, ImageTagRepositoryCustom {
+
+    @Query("select t.id from ImageTag t where t.image.id = :imageId")
     List<Long> findImageTagIdsByImageId(Long imageId);
 }
