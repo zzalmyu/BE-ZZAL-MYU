cf9a8d0df5fc6a5b220e25fa47b793c8d39e2e40
SeungSoo So <63526857+voidmelody@users.noreply.github.com>
2024-02-20 20:44:37 +0900

    [FEAT] cors 설정 (#75)

---
+++ src/main/java/com/prgrms/zzalmyu/core/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.prgrms.zzalmyu.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173", "http://localhost:8080", "http://localhost:80")
+                .allowedMethods("GET", "POST", "DELETE")
+                .allowCredentials(true)
+                .maxAge(3000);
+    }
+}
