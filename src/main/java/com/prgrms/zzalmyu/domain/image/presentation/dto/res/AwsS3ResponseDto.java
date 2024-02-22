1199aed28114f41e43f28b4a9189e22beba6a5f2
voidmelody <sss9073@naver.com>
2024-02-22 16:53:24 +0900

    [FEAT] 이미지 response Dto에 제목 추가

--- src/main/java/com/prgrms/zzalmyu/domain/image/presentation/dto/res/AwsS3ResponseDto.java
+++ src/main/java/com/prgrms/zzalmyu/domain/image/presentation/dto/res/AwsS3ResponseDto.java
@@ -8,17 +8,18 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class AwsS3ResponseDto {
-
     private Long imageId;
+    private String title;
     private String path;
 
     @Builder
-    private AwsS3ResponseDto(Long imageId, String path) {
+    private AwsS3ResponseDto(Long imageId, String title, String path) {
         this.imageId = imageId;
+        this.title = title;
         this.path = path;
     }
 
     public AwsS3ResponseDto(Image image) {
-        this(image.getId(), image.getPath());
+        this(image.getId(), image.getTitle(), image.getPath());
     }
 }
