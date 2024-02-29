ef55a76bd9ea421ea33da3661681b50b1addffb2
SeungSoo So <63526857+voidmelody@users.noreply.github.com>
2024-02-29 18:41:04 +0900

    [FEAT] 업로드 및 좋아요 누른 사진에서 태그 필터링 검색 시 tagCount 증가로직 추가 (#127)

--- src/main/java/com/prgrms/zzalmyu/domain/image/application/ImageSearchService.java
+++ src/main/java/com/prgrms/zzalmyu/domain/image/application/ImageSearchService.java
@@ -1,5 +1,6 @@
 package com.prgrms.zzalmyu.domain.image.application;
 
+import com.prgrms.zzalmyu.domain.image.presentation.dto.req.TagSearchRequestDto;
 import com.prgrms.zzalmyu.domain.image.presentation.dto.res.AwsS3ResponseDto;
 import com.prgrms.zzalmyu.domain.user.domain.entity.User;
 
@@ -10,14 +11,12 @@ public interface ImageSearchService {
     /**
      * 좋아요한 짤 태그 필터링
      *
-     * @param tagIdList
      */
-    List<AwsS3ResponseDto> searchLikeImages(User user, List<Long> tagIdList);
+    List<AwsS3ResponseDto> searchLikeImages(User user, TagSearchRequestDto tagSearchRequestDto);
 
     /**
      * 업로드한 짤 태그 필터링
      *
-     * @param tagIdList
      */
-    List<AwsS3ResponseDto> searchUploadImages(User user, List<Long> tagIdList);
+    List<AwsS3ResponseDto> searchUploadImages(User user, TagSearchRequestDto tagSearchRequestDto);
 }
