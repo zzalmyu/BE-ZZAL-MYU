0527ba281bf1a3073afb8dd304b60fb268f45b65
suzzingv <84590065+suzzingv@users.noreply.github.com>
2024-03-02 17:36:51 +0900

    [REFACT] me 관련 응답 분리

--- src/main/java/com/prgrms/zzalmyu/domain/tag/infrastructure/TagRepositoryCustom.java
+++ src/main/java/com/prgrms/zzalmyu/domain/tag/infrastructure/TagRepositoryCustom.java
@@ -1,5 +1,6 @@
 package com.prgrms.zzalmyu.domain.tag.infrastructure;
 
+import com.prgrms.zzalmyu.domain.tag.presentation.dto.res.TagMeResponseDto;
 import com.prgrms.zzalmyu.domain.tag.presentation.dto.res.TagResponseDto;
 
 import java.util.List;
@@ -7,8 +8,8 @@ import java.util.List;
 public interface TagRepositoryCustom {
 
     List<TagResponseDto> getTopTagsFromUserUsed(int limit);
-    List<TagResponseDto> getTopTagsFromLikedImages(Long userId, int limit);
-    List<TagResponseDto> getTopTagsFromUploadImages(Long userId, int limit);
+    List<TagMeResponseDto> getTopTagsFromLikedImages(Long userId, int limit);
+    List<TagMeResponseDto> getTopTagsFromUploadImages(Long userId, int limit);
     List<TagResponseDto> searchTagForAutoSearchName(String inputString);
     List<TagResponseDto> searchTagForAutoSearchNameFromLikeImages(Long userId, String inputString);
     List<TagResponseDto> searchTagForAutoSearchNameFromUploadImages(Long userId, String inputString);
