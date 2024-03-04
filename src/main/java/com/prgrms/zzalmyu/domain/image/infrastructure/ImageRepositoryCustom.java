0fd4bb1a6c68303f5929aafda24beb4d00f1a8ef
YongNyeo <won05121@gmail.com>
2024-03-04 10:47:11 +0900

    [REFACT] 변수를 받기 위해 querydsl로 변경

--- src/main/java/com/prgrms/zzalmyu/domain/image/infrastructure/ImageRepositoryCustom.java
+++ src/main/java/com/prgrms/zzalmyu/domain/image/infrastructure/ImageRepositoryCustom.java
@@ -1,10 +1,14 @@
 package com.prgrms.zzalmyu.domain.image.infrastructure;
 
+import com.prgrms.zzalmyu.domain.image.domain.entity.Image;
 import com.prgrms.zzalmyu.domain.image.presentation.dto.res.ImageResponseDto;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ImageRepositoryCustom {
     List<ImageResponseDto> getTopUserUsedImage(Pageable pageable);
+
+    List<Image> findTopImageLike(int limit);
 }
