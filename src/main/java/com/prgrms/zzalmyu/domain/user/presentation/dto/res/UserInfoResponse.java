08cbc805c81150295361b89272d1bebff594310d
YongNyeo <won05121@gmail.com>
2024-03-05 23:17:47 +0900

    [FEAT] User info 반환하는 API 추가

---
+++ src/main/java/com/prgrms/zzalmyu/domain/user/presentation/dto/res/UserInfoResponse.java
@@ -0,0 +1,24 @@
+package com.prgrms.zzalmyu.domain.user.presentation.dto.res;
+
+import com.prgrms.zzalmyu.domain.user.domain.entity.User;
+import com.prgrms.zzalmyu.domain.user.domain.enums.Role;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class UserInfoResponse {
+    private Long userId;
+    private String email;
+    private Role role;
+
+    private UserInfoResponse(Long userId, String email, Role role) {
+        this.userId = userId;
+        this.email = email;
+        this.role = role;
+    }
+    public static UserInfoResponse of(User user) {
+        return new UserInfoResponse(user.getId(), user.getEmail(), user.getRole());
+    }
+}
