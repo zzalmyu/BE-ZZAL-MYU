455e66feba2d4f5a955c5823ced7a4a6d50ef7de
voidmelody <sss9073@naver.com>
2024-03-06 13:19:49 +0900

    [FEAT] 좋아요 / 업로드 한 이미지 태그 필터링 기능 구현

--- src/main/java/com/prgrms/zzalmyu/domain/image/application/ImageSearchService.java
+++ src/main/java/com/prgrms/zzalmyu/domain/image/application/ImageSearchService.java
@@ -1,6 +1,5 @@
 package com.prgrms.zzalmyu.domain.image.application;
 
-import com.prgrms.zzalmyu.domain.image.presentation.dto.req.TagSearchRequestDto;
 import com.prgrms.zzalmyu.domain.image.presentation.dto.res.AwsS3ResponseDto;
 import com.prgrms.zzalmyu.domain.user.domain.entity.User;
 
@@ -12,11 +11,11 @@ public interface ImageSearchService {
      * 좋아요한 짤 태그 필터링
      *
      */
-    List<AwsS3ResponseDto> searchLikeImages(User user, TagSearchRequestDto tagSearchRequestDto);
+    List<AwsS3ResponseDto> searchLikeImages(User user, List<String> tagNames);
 
     /**
      * 업로드한 짤 태그 필터링
      *
      */
-    List<AwsS3ResponseDto> searchUploadImages(User user, TagSearchRequestDto tagSearchRequestDto);
+    List<AwsS3ResponseDto> searchUploadImages(User user, List<String> tagNames);
 }
