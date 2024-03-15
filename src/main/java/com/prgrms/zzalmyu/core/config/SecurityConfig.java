c0c19afc6d01550af9b47f784e526fb10d4e2b06
suzzingv <sjlim1999@naver.com>
2024-03-15 17:16:35 +0900

    [CHORE] security 접근 권한 설정

--- src/main/java/com/prgrms/zzalmyu/core/config/SecurityConfig.java
+++ src/main/java/com/prgrms/zzalmyu/core/config/SecurityConfig.java
@@ -57,7 +57,8 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.GET, "/api/v1/image", "/api/v1/image/{imageId}").permitAll()
                 .requestMatchers("/api/v1/image/**").authenticated()
                 .requestMatchers("/api/v1/chat").permitAll()
-                .requestMatchers("/chat").permitAll())
+                .requestMatchers("/chat").permitAll()
+                    .requestMatchers("/api/v1/login").permitAll())
             .exceptionHandling(customizer -> customizer
                 .authenticationEntryPoint(customAuthenticationEntryPoint())
                 .accessDeniedHandler(customAccessDeniedHandler()))
